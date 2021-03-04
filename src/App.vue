6f49816b0e132abb33fd3656a3b510aea044cbd5
Arunkumar Sivaramasethu <arunkumar1.s@intellectdesign.com>
2021-03-04 12:39:59 +0530

    changing submodule location and name

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <IdxButton dense outlined v-model="value" title="Submit" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import IdxButton from  './assets/idxUILibrary/src/components/IdxButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    IdxButton
   }
 }
 </script>
